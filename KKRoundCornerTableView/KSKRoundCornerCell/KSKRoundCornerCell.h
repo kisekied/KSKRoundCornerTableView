43cbfe519c62f46a8d381ecd753096310a4fa76e
kisekied <kisekied@gmail.com>
2016-02-03 17:29:41 +0800

    KSKRoundCornerCell

---
+++ KKRoundCornerTableView/KSKRoundCornerCell/KSKRoundCornerCell.h
@@ -0,0 +1,13 @@
+//
+//  KSKRoundCornerCell.h
+//  KKRoundCornerTableView
+//
+//  Created by kisekied on 16/2/3.
+//  Copyright © 2016年 kisekied. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSKRoundCornerCell : UITableViewCell
+
+@end
---
+++ KKRoundCornerTableView/KSKRoundCornerCell/KSKRoundCornerCell.m
@@ -0,0 +1,23 @@
+//
+//  KSKRoundCornerCell.m
+//  KKRoundCornerTableView
+//
+//  Created by kisekied on 16/2/3.
+//  Copyright © 2016年 kisekied. All rights reserved.
+//
+
+#import "KSKRoundCornerCell.h"
+
+@implementation KSKRoundCornerCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
